a94a34e69dceb78d757637b487fd0432fb990a38
Kinshuk Bairagi <me@kinshuk.in>
2017-01-07 22:11:22 +0530

    Adding about page

--- src/index.js
+++ src/index.js
@@ -4,12 +4,14 @@ import { Provider } from 'react-redux';
 import store from './stores/dataStore'
 import { Router, Route, Link, browserHistory, IndexRoute } from 'react-router';
 import App from './App';
+import About from './components/about';
 import './index.css';
 
 render((
 <Provider store={store}>
   <Router history={browserHistory}>
     <Route path="/" component={App} >
+      <Route path="/about" component={About} />
     </Route>
   </Router>
 </Provider>
