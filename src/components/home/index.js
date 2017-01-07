4714094679683db734be0beda24d48ead465398a
anshul.shukla <anshul.shukla@flipkart.com>
2017-01-07 23:55:22 +0530

    imported images.

--- src/components/home/index.js
+++ src/components/home/index.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
 import { Router, Route, Link, browserHistory } from 'react-router';
+import template1 from './../../img/temple1.jpg';
+import template2 from './../../img/temple2.jpg';
 
 
 class Home extends Component {
   render() {
     return (
-      <div className="home">
-        <div>
+      <div>
+        <div className="home">
         <form>
             <input type="text" placeholder="Enter Hotel or place name in Bundelkhand e.g. Khajuraho"/>
             <button type="submit">Search</button>
@@ -16,7 +18,7 @@ class Home extends Component {
           <div className="experience-header">Experience <span className="bundelkhand">Bundelkhand</span> </div>
           <div className="experienceBox">
                 <div className="experienceImageBox">
-                    <img src="img/temple1.jpg" />
+                    <img src={template1} height="250"/>
                 </div>
                 <div className="experienceTextBox">
                   Experience Orchha Temples
@@ -24,7 +26,7 @@ class Home extends Component {
           </div>
            <div className="experienceBox">
                 <div className="experienceImageBox">
-                <img src="img/temple2.jpg" />
+                <img src={template2} height="250"/>
                 </div>
                 <div className="experienceTextBox">
                   Experience Khajuraho Temples
